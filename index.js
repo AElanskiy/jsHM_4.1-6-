2c06452ed7a41465484e0fcc970ad13cc0665431
AElanskiy <elan.it.acc@gmail.com>
2022-10-19 18:19:31 +0300

    Create repository for homework

---
+++ index.js
@@ -0,0 +1,9 @@
+let firstNumber = +prompt('Your first number');
+let secondNumber = +prompt('Your second number');
+
+let add = firstNumber + secondNumber ;
+let sub = firstNumber - secondNumber ;
+let mult = firstNumber * secondNumber ;
+let div = firstNumber / secondNumber ;
+
+alert(`$(firstNumber)+$(secondNumber)=$(add) \n$(firstNumber)-$(secondNumber)=$(sub) \n$(firstNumber)*$(secondNumber)=$(mult) \n$(firstNumber)/$(secondNumber)=$(div)`);
